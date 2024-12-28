40fe50c2efb79b2a60ecec71f215c6548f5e18d3
hrishikasamani <samani.hr@northeastern.edu>
2024-12-27 21:08:23 -0500

    final

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
 import Lights from './Lights';
 import * as THREE from 'three';
+import Iphone from './Iphone';
+import Loader from "./Loader";
 
 // import Loader from './Loader';
 
@@ -10,7 +12,7 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotation, size, it
         <View
             index={index}
             id={gsapType}
-            className={`w-full h-full ${index===2} ? 'right-[-100%] : ''`}
+            className={`w-full h-full absolute ${index===2 ? 'right-[-100%]' : ''}`}
         >
             <ambientLight intensity={0.3}/>
             <PerspectiveCamera makeDefault position={[0, 0, 4]} />
@@ -22,10 +24,15 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotation, size, it
                 enablePan={false}
                 rotateSpeed={0.4}
                 target={new THREE.Vector3(0, 0, 0)}
+                onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
             />
             <group ref={groupRef} name={`${index===1} ? 'small' : 'large'`} position={[0, 0, 0]}>
-                <Suspense fallback={<Html><div>Loading</div></Html>}>
-                    {/* <IPhone/> */}
+                <Suspense fallback={<Loader />}>
+                    <Iphone
+                        scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+                        item={item}
+                        size={size}
+                    />
                 </Suspense>
             </group>
 
